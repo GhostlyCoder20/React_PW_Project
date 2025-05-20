033bca0bd78cb7512666c816abbf71b40f9c777a
GhostlyCoder20 <fjmrcoder20@gmail.com>
2025-05-20 15:24:27 -0600

    railway connection

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// let url_api = "http://localhost:3000/";
+let url_api = "https://pwmoviepoject-production.up.railway.app/";
+
 const api = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: url_api,
     headers: {
         'Content-Type': 'application/json'
     }
